9e1b1f0921c1ce1c56e0c5e5bff048df00c00b05
Jared M Schooley <jaschoo@siue.edu>
2019-01-24 15:43:42 -0600

    Various code formatting changes, comments, etc

--- html/actionIsComing.php
+++ html/actionIsComing.php
@@ -7,6 +7,14 @@
  * Time: 2:07 AM
  */
 
+/**
+ * Updated by IntelliJ
+ * User: jaredmschooley
+ * Author: Jared M Schooley
+ * Date: 1/24/19
+ * Time: 3:14 PM
+ */
+
 include("connectDB.php");
 include("email.php");
 include("redirect.php");
@@ -18,8 +26,8 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     //ini_set('display_errors', 'On');
     //error_reporting(E_ALL);
 
-//This gets all the other information from the form
-//Writes the Filename to the server
+    //This gets all the other information from the form
+    //Writes the Filename to the server
 
     //$firstName = $_POST['firstText'];
     $firstName = strip_tags($_POST['firstText']);
@@ -42,12 +50,6 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $lastName = str_replace('\'', '', $lastName);
     $file = str_replace('\'', '', $file);
     $file = str_replace('\"', '', $file);
-    //$alteredFirst = $firstName;
-    //$alteredLast = $lastName;
-    //preg_replace("/(\W)+/", "", $alteredFirst);
-    //preg_replace("/(\W)+/", "", $alteredLast);
-    //$firstName = $alteredFirst;
-    //$lastName = $alteredLast;
 
     $target = "2019Documents/";
     $target = $target . $firstName . $lastName . '/';
@@ -55,8 +57,9 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $dir = "2019Documents/";
     $dir = $dir . $firstName . $lastName;
     mkdir($dir, 0777, true); //create the directory
-//This gets all the other information from the form
-//Writes the Filename to the server
+
+    //This gets all the other information from the form
+    //Writes the Filename to the server
     if (move_uploaded_file($_FILES['resumeUpload']['tmp_name'], $target)) {
         if (empty($_POST['bus'])) {
             $bus = 'N/A';
@@ -65,8 +68,6 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
             $bus = $_POST['bus'];
             $doBus = true;
         }
-
-//print_r($eduIns);
 
         echo REDIRECT;
 
@@ -77,6 +78,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         echo "WARNING: There was an error uploading your file, please try again.";
         echo "Maybe the file name has already been uploaded, or the file is too large.";
     }
+
 } else {
     echo "Something went wrong, Please resubmit form";
 }
@@ -93,44 +95,31 @@ function redirect()
     exit();
 }
 
+//Connects to DB and writes data to database & table.
 function connect($first, $last, $email, $edu, $eth, $gender, $shirt, $file, $bus)
 {
-
-    //$phone = "000-000-0000";
-    //$allergies = "NUL";
-    //$allDesc = "NONE";
-    //$file = "NULL";
-
     $conn = new mysqli(DB_HOST, DB_USR, DB_PWD);
     $conn->select_db(DB_NAME);
-// Check connection
+    // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    //  print_r("CONNECTED SUCCESSFULLY");
-
-    // $test = ("SELECT * FROM ehacks2018_registration");
-    // print_r($conn->query($test));
     $insertData = "INSERT INTO ehacks2019_registration(
                           First_Name, Last_Name, Email, School, Gender, Race, Shirt_Size, File, Bus)
                    VALUES ('$first','$last','$email','$edu','$gender','$eth','$shirt','$file','$bus')";
 
-
-    //$insertData = "INSERT INTO ehacks2018_registration ()
-    //              VALUES ('$first', '$last', '$email', '$phone', '$edu', '$gender', '$eth', '$shirt', '$allergies', '$allDesc', '$file');";
-
     $conn->query($insertData);
 }
 
+//Handles sending out confirmation email.
 function emailReg($firstName, $lastName, $email, $edu, $doBus)
 {
 
     $headers = "From: no-reply@ehacks.cs.siue.edu" . "\r\n";
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-    // $admin_email = "no-reply@ehacks.cs.siue.edu";
-    $subject = "Ehacks 2019 Registration";
+    $subject = "eHacks 2019 Registration";
 
     if ($doBus) {
         $message = '<html><body>';
--- html/js/registrationJS.js
+++ html/js/registrationJS.js
@@ -3,104 +3,87 @@ function setup() {
 
     function openDialog() {
         document.getElementById('fileUpload').click();
-
-        // showFileName();
     }
 
     document.getElementById('fileUpload').onchange = function () {
-        var fullPath = document.getElementById('fileUpload').value;
+        let fullPath = document.getElementById('fileUpload').value;
         if (fullPath) {
-            var startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
-            var filename = fullPath.substring(startIndex);
+            let startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
+            let filename = fullPath.substring(startIndex);
             if (filename.indexOf('\\') === 0 || filename.indexOf('/') === 0) {
                 filename = filename.substring(1);
             }
             document.getElementById('fileName').innerHTML = filename;
         }
-        //document.getElementById("fileName").style.borderBottomStyle = "thin solid #000000";
-        //document.getElementById("fileName").style.borderBottomWidth = "100px";
         document.getElementById('fileName').style.border = "thin solid #000000";
         document.getElementById('fileName').style.borderRadius = "5px";
         document.getElementById('fileName').style.padding = "2px";
         document.getElementById('fileName').style.marginLeft = "0px";
     }
-    // document.getElementById('firstName').style.width = "500px";
-    // document.getElementById('firstName').style.width = "500px";
-    // document.getElementById('lastName').style.width = "500px";
-    // document.getElementById('email').style.width = "500px";
-    // document.getElementById('educational').style.width = "500px";
-    // document.getElementById('ethnicity').style.width = "500px";
-    // document.getElementsByName('gender').style.width = "500px";
-    // document.getElementsByName('shirtSize').style.width = "500px";
-    // document.getElementById('errorSection').style.width = "500px";
-    // document.getElementById('fileUpload').style.width = "500px";
-    // document.getElementById('buttonDiv').style.width = "200px";
 }
 
 <!--School Selection JS-->
-    function showfield(name) {
-        if (name === 'Other') {
-            document.getElementById('eduError').innerHTML = '<input type="text" id="otherSchool" name="otherSchoolText" placeholder="Enter Other Institution"' +
-                'class="form-control" autofocus required />' +
-                '<span class="help-block">Full Educational Institution Name, eg.: Southern Illinois University Edwardsville</span>';
-        } else if (name === 'SIUE') {
-            document.getElementById('eduError').innerHTML = '<span>Please note; If you are a student of SIUE, we will be providing ' +
-                'a bus to get you to and from eHacks (The T-Rex building). If you are interested in taking advantage of this option please let us know below.</span>' +
-                '<div class="form-group">' +
-                '<label class="control-label col-sm-3">Will you make use of the bus?</label>' +
-                '<div class="col-sm-6">' +
-                '<div class="row">' +
-                '<div class="col-sm-4">' +
-                '<label class="radio-inline">' +
-                '<input type="radio" name="bus" id="yesRadio" value="Yes" required />Yes' +
-                '</label>' +
-                '</div>' +
-                '<div class="col-sm-4">' +
-                '<label class="radio-inline">' +
-                '<input type="radio" name="bus" id="noRadio" value="No" required />No' +
-                '</label>' +
-                '</div>' +
-                '</div>' +
-                '</div>' +
-                '</div>';
-        }
-        else {
-            document.getElementById('eduError').innerHTML = '';
-        }
+function showfield(name) {
+    if (name === 'Other') {
+        document.getElementById('eduError').innerHTML = '<input type="text" id="otherSchool" ' +
+            'name="otherSchoolText" placeholder="Enter Other Institution" class="form-control" autofocus required />' +
+            '<span class="help-block">Full Educational Institution Name, eg.: Southern Illinois University Edwardsville</span>';
+    } else if (name === 'SIUE') {
+        document.getElementById('eduError').innerHTML =
+            '<span>Please note; If you are a student of SIUE, we will be providing ' +
+            'a bus to get you to and from eHacks (The T-Rex building). If you are interested ' +
+            'in taking advantage of this option please let us know below.</span>' +
+            '<div class="form-group">' +
+            '<label class="control-label col-sm-3">Will you make use of the bus?</label>' +
+            '<div class="col-sm-6">' +
+            '<div class="row">' +
+            '<div class="col-sm-4">' +
+            '<label class="radio-inline">' +
+            '<input type="radio" name="bus" id="yesRadio" value="Yes" required />Yes' +
+            '</label>' +
+            '</div>' +
+            '<div class="col-sm-4">' +
+            '<label class="radio-inline">' +
+            '<input type="radio" name="bus" id="noRadio" value="No" required />No' +
+            '</label>' +
+            '</div>' +
+            '</div>' +
+            '</div>' +
+            '</div>';
+    } else {
+        document.getElementById('eduError').innerHTML = '';
     }
+}
 
-//function functSubmit(event) {
-//    alert("REGISTADA");
-//}
 <!--/School js-->
 
 <!--Validation JS-->
-var isValid = false;
-var count = 0;
+let isValid = false;
+let count = 0;
 
 function validate() {
 
-    var alphaNum = /^[0-9a-zA-Z]+$/;
+    let alphaNum = /^[0-9a-zA-Z]+$/;
 
-    var firstValid = false;
-    var lastValid = false;
-    var emailValid = false;
-    var eduValid = false;
-    var ethValid = false;
-    var genValid = false;
-    var shirtValid = false;
-    var resumeValid = false;
+    let firstValid = false;
+    let lastValid = false;
+    let emailValid = false;
+    let eduValid = false;
+    let ethValid = false;
+    let genValid = false;
+    let shirtValid = false;
+    let resumeValid = false;
 
-    var inpObj = document.getElementById('firstName');
-    var lastObj = document.getElementById('lastName');
-    var emailObj = document.getElementById('email');
-    var eduObj = document.getElementById('educational');
-    var ethObj = document.getElementById('ethnicity');
-    var genderObj = document.getElementsByName('gender');
-    var shirtObj = document.getElementsByName('shirtSize');
-    var errorSec = document.getElementById('errorSection');
-    var fileObj = document.getElementById('fileUpload');
-    var errorMessage = '';
+    let inpObj = document.getElementById('firstName');
+    let lastObj = document.getElementById('lastName');
+    let emailObj = document.getElementById('email');
+    let eduObj = document.getElementById('educational');
+    let ethObj = document.getElementById('ethnicity');
+    let genderObj = document.getElementsByName('gender');
+    let shirtObj = document.getElementsByName('shirtSize');
+    let errorSec = document.getElementById('errorSection');
+    let fileObj = document.getElementById('fileUpload');
+    let errorMessage = '';
 
     if (!inpObj.checkValidity()) {
         inpObj.setAttribute("placeholder", "Required Field - Please enter first name");
@@ -145,10 +128,11 @@ function validate() {
 
     if (eduObj.selectedIndex === 0) {
         errorMessage += '<span style="color:red">Required Field - Please select educational institution.</span><br>';
-        document.getElementById('eduError').innerHTML = '<span style="color:red">Select valid educational institution.</span><br>';
+        document.getElementById('eduError').innerHTML =
+            '<span style="color:red">Select valid educational institution.</span><br>';
         eduValid = false;
     } else if (eduObj.selectedIndex === 9) {
-        var otherSchoolInput = document.getElementById('otherSchool');
+        let otherSchoolInput = document.getElementById('otherSchool');
 
         if (!otherSchoolInput.checkValidity()) {
             otherSchoolInput.setAttribute("placeholder", "Required Field - Please enter other school");
@@ -158,14 +142,14 @@ function validate() {
             eduValid = false;
         } else {
             eduValid = true;
-            var otherSet = document.getElementById('otherSchool').value;
-            eduObj[9].value = otherSet;
+            eduObj[9].value = document.getElementById('otherSchool').value;
         }
     } else if (eduObj.selectedIndex === 1) {
-        var selectedBusOp = document.getElementsByName('bus');
+        let selectedBusOp = document.getElementsByName('bus');
         if (!(selectedBusOp[0].checked || selectedBusOp[1].checked)) {
             if (count === 0) {
-                document.getElementById('eduError').innerHTML += '<span style="color:red">Please select yes or no</span><br>';
+                document.getElementById('eduError').innerHTML +=
+                    '<span style="color:red">Please select yes or no</span><br>';
                 count++;
             }
             errorMessage += '<span style="color:red">Required Field - Please select a bus option.</span><br>';
@@ -173,12 +157,14 @@ function validate() {
         } else {
             eduValid = true;
             if (count > 0) {
-                var checked;
+                let checked;
                 checked = document.getElementById('yesRadio').checked;
 
                 document.getElementById('eduError').innerHTML = '';
-                document.getElementById('eduError').innerHTML = '<span>Please note; If you are a student of SIUE, we will be providing ' +
-                    'a bus to get you to and from eHacks (The T-Rex building). If you are interested in taking advantage of this option please let us know below.</span>' +
+                document.getElementById('eduError').innerHTML =
+                    '<span>Please note; If you are a student of SIUE, we will be providing ' +
+                    'a bus to get you to and from eHacks (The T-Rex building). If you are ' +
+                    'interested in taking advantage of this option please let us know below.</span>' +
                     '<div class="form-group">' +
                     '<label class="control-label col-sm-3">Will you make use of the bus?</label>' +
                     '<div class="col-sm-6">' +
@@ -198,11 +184,11 @@ function validate() {
                     '</div>';
 
                 if (checked === true) {
-                    var yesCheck = document.getElementById("yesRadio");
+                    let yesCheck = document.getElementById("yesRadio");
                     yesCheck.value = "Yes";
                     yesCheck.checked = true;
                 } else if (checked === false) {
-                    var noCheck = document.getElementById('noRadio');
+                    let noCheck = document.getElementById('noRadio');
                     noCheck.value = "No";
                     noCheck.checked = true;
                 }
@@ -214,7 +200,8 @@ function validate() {
     }
 
     if (ethObj.selectedIndex === 0) {
-        errorMessage += '<span style="color:red">Required Field - Please select ethnicity - If you wish not to answer select "Prefer Not to Say".</span><br>';
+        errorMessage += '<span style="color:red">' +
+            'Required Field - Please select ethnicity - If you wish not to answer select "Prefer Not to Say".</span><br>';
         document.getElementById('ethError').innerHTML = '<span style="color:red">Please select ethnicity</span><br>';
         ethValid = true
     } else {
@@ -223,7 +210,8 @@ function validate() {
     }
 
     if (!(genderObj[0].checked || genderObj[1].checked || genderObj[2].checked || genderObj[3].checked)) {
-        errorMessage += '<span style="color:red">Required Field - Please select gender - If you wish not to answer select "Prefer Not to Say".</span><br>';
+        errorMessage += '<span style="color:red">' +
+            'Required Field - Please select gender - If you wish not to answer select "Prefer Not to Say".</span><br>';
         document.getElementById('genderError').innerHTML = '<span style="color:red">Please select gender field.</span><br>';
         genValid = false;
     } else {
